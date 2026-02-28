32dcd143724bfbaa72ea153a93ce1fee2c7946af
Kirschtaria06 <187531836+Kirschtaria06@users.noreply.github.com>
2026-02-28 20:31:36 +0800

    Finished upto 1.11

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,39 @@ import { useState } from "react"
 
 const Button = ({ onclick, text }) => <button onClick = {onclick}>{text}</button>
 
-const Statistics = ({ good, neutral, bad}) => {
-    const total = good + neutral + bad
-    let average = (good - bad) / total;
-    let positivePercentage = (good / total) * 100
-    if (Number.isNaN(average) && Number.isNaN(positivePercentage)){
-      average = 0
-      positivePercentage = 0
-    }
-    return (
-      <>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive Percentage: {positivePercentage.toFixed(2) + "%"}</p>
-      </>
-    )
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+
+  const average = (good - bad) / total
+  const positivePercentage = (good / total) * 100
+
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine text="Good" value={good} />
+        <StatisticsLine text="Neutral" value={neutral} />
+        <StatisticsLine text="Bad" value={bad} />
+        <StatisticsLine text="All" value={total} />
+        <StatisticsLine text="Average" value={average} />
+        <StatisticsLine text="Positive" value={positivePercentage.toFixed(2) + "%"} />
+      </tbody>
+    </table>
+  )
 }
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const App = () => {
   const [feedback, setFeedback] = useState({good: 0, neutral: 0, bad: 0})
   const { good, neutral, bad } = feedback
@@ -36,12 +49,26 @@ const App = () => {
     setFeedback({...feedback, bad: bad + 1})
   }
 
+  if (good === 0 && neutral === 0 && bad === 0){
+    return (
+      <>
+      <h1>Give feedback</h1>
+      <Button onclick={handleGood} text={"Good"}/>
+      <Button onclick={handleNeutral} text={"Neutral"}/>
+      <Button onclick={handleBad} text={"Bad"}/>
+      <h1>Statistics</h1>
+      <h3>No feedback given!</h3>
+      </>
+    )
+  }
+
   return (
     <>
     <h1>Give feedback</h1>
     <Button onclick={handleGood} text={"Good"}/>
     <Button onclick={handleNeutral} text={"Neutral"}/>
     <Button onclick={handleBad} text={"Bad"}/>
+    <h1>Statistics</h1>
     <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
